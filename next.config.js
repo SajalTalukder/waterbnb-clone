95e4062926b82057cbc23aebd69e6944f6c41082
sajal talukder <sajaltalkder650@gmail.com>
2022-04-06 16:55:09 +0600

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    mapbox_key:
-      "pk.eyJ1Ijoic2FqYWwxMjM0IiwiYSI6ImNsMW02Y3FhejBhbm8zYm54Zm9kbnZtMXkifQ.sdAiJbWczEY_gGsQeoz9qA",
-  },
 };
 
 module.exports = nextConfig;
